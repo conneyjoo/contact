715fdf1337fcbd62bc3342ef7772eb6e8d213746
conneyjoo <conneyjoo@outlook.com>
2017-11-10 20:10:52 +0800

    11

--- src/main/java/com/jinshun/contact/utils/JsonUtils.java
+++ src/main/java/com/jinshun/contact/utils/JsonUtils.java
@@ -6,41 +6,29 @@ import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 
-public class JsonUtils
-{
-	public static String toJson(Object obj)
-	{
-		try
-		{
-			return new ObjectMapper().writeValueAsString(obj);
-		}
-		catch (JsonProcessingException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-	}
+public class JsonUtils {
 
-	public static <T> T toObject(String json, Class<T> type)
-	{
-		try
-		{
-			return (T) new ObjectMapper().readValue(json, type);
-		}
-		catch (JsonProcessingException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-	}
+    public static String toJson(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static <T> T toObject(String json, Class<T> type) {
+        try {
+            return (T) new ObjectMapper().readValue(json, type);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
