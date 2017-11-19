7c91f553f3c79b70346b4cf8e95de2cf35a0cc17
conneyjoo <conneyjoo@outlook.com>
2017-11-19 16:12:16 +0800

    提交权限

---
+++ src/main/resources/static/warehouse/bidWarehouse-upload.js
@@ -0,0 +1,29 @@
+var id = $.util.urlParam('id');
+var type = $.util.urlParam('type');
+
+$.ajax({
+    type: 'POST',
+    url: '/file/getFiles',
+    data: {bidId: id, type: type},
+    success: function(files) {
+        var file;
+
+        for (var i = 0, len = files.length; i < len; i++) {
+            file = files[i];
+            uploader.loadFile(file.id, file.name, file.path, '/../images/' + file.path);
+        }
+    },
+    error: function(msg) {
+        alert('上传失败');
+    }
+});
+
+var uploader = $('#uploadPicture').uploader({
+    autoUpload: true,
+    chunk_size: 10485760,
+    url: '/uploadTmp',
+    deleteActionOnDone: function(file, doRemoveFile) {
+    },
+}).on('onBeforeUpload', function(event, file) {
+}).on('onFileUploaded', function(event, file) {
+}).data('zui.uploader');
