a97189a4af50519fe6671e56eb26ba820721bb32
hangjun1984 <f8998773259>
2017-12-03 16:55:05 +0800

    upload bid code

--- src/main/resources/static/contact/bid-upload.js
+++ src/main/resources/static/contact/bid-upload.js
@@ -12,6 +12,8 @@ $.ajax({
             file = files[i];
             uploader.loadFile(file.id, file.name, file.path, '/../images/' + file.path);
         }
+        $('.btn-delete-file').addClass('role').attr('data-permission', '65536');
+        loadPermission();
     },
     error: function(msg) {
         alert('上传失败');
